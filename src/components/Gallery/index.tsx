96171a7e8f118136c2b5e694822fb62e5154326b
DevPBDias <devpbdias@gmail.com>
2025-07-18 11:16:07 -0300

    feat: add content to gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
-import gallery1 from "@/assets/3x3.png";
-import gallery2 from "@/assets/basket_plays.png";
-import gallery3 from "@/assets/woman_player.png";
-import gallery4 from "@/assets/kids_playing.png";
+import gallery1 from "@/assets/gallery/campeonatos-2.png";
+import gallery2 from "@/assets/gallery/campeonatos-3.png";
+import gallery3 from "@/assets/gallery/mvp-2.png";
+import gallery4 from "@/assets/gallery/ftb.png";
+import gallery5 from "@/assets/gallery/social.png";
 
 const PhotosGallery = () => {
   return (
@@ -18,24 +19,32 @@ const PhotosGallery = () => {
             className="w-full h-full object-cover rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out"
           />
         </picture>
-        <div className="lg:col-start-2 lg:col-end-3 lg:row-start-1 lg:row-end-3  rounded-lg">
+        <div className="lg:col-start-1 lg:col-end-2 lg:row-start-2 lg:row-end-3 rounded-lg">
           <Image
             src={gallery2}
             alt="Galeria 2"
             className="w-full h-full object-cover rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out"
           />
         </div>
-        <div className="lg:col-start-3 lg:col-end-4 lg:row-start-1 lg:row-end-3 rounded-lg">
+
+        <div className="md:row-start-1 md:row-end-3 md:col-start-1 md:col-end-2 lg:col-start-2 lg:col-end-3 lg:row-start-1 lg:row-end-3 rounded-lg">
           <Image
             src={gallery3}
+            alt="Galeria 4"
+            className="w-full h-full object-cover rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out"
+          />
+        </div>
+        <div className="md:row-start-2  md:row-end-3 md:col-start-2 md:col-end-3 lg:col-start-3 lg:col-end-4 lg:row-start-1 lg:row-end-2  rounded-lg">
+          <Image
+            src={gallery4}
             alt="Galeria 3"
             className="w-full h-full object-cover rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out"
           />
         </div>
-        <div className="lg:col-start-1 lg:col-end-2 lg:row-start-2 lg:row-end-3 rounded-lg">
+        <div className="md:row-start-3 md:row-end-4 md:col-start-2 md:col-end-3 lg:col-start-3 lg:col-end-4 lg:row-start-2 lg:row-end-3 rounded-lg">
           <Image
-            src={gallery4}
-            alt="Galeria4"
+            src={gallery5}
+            alt="Galeria 5"
             className="w-full h-full object-cover rounded-lg hover:scale-105 transition-transform duration-300 ease-in-out"
           />
         </div>
